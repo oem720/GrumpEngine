321d6fc759cfbeb54e6b0d97086e8c3257e18b5d
Cameron Taylor <CDTaylor2@student.fullsail.edu>
2020-12-14 22:44:27 -0500

    Added talking

--- GrumpEngine/Tester.cs
+++ GrumpEngine/Tester.cs
@@ -10,6 +10,13 @@ namespace GrumpEngine
     {
         static void Main(string[] args)
         {
+
+            string text = "In the beginning. There was a Grump. A BIG Grump!!\nOwen didn't know how to encounter this problem because he was too smooth brain. So he woke up with an idea!\nHe grabbed his boots and ran out the door. Hopped on his bike and rode off a cliff.\nNow Owen is no longer smooth brain.";
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                System.Threading.Thread.Sleep(50);
+            }
             ResizeableMatrix rm = new ResizeableMatrix(6, 3);
 
             rm.Fill(27);
