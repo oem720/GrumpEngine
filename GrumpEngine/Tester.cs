41a0bbb7b8ec8adbc07d231d99a6a2932b661bdf
Owen Meyers <OEMeyers@student.fullsail.edu>
2020-12-15 00:09:59 -0500

    Added the top layer form class to display text

--- GrumpEngine/Tester.cs
+++ GrumpEngine/Tester.cs
@@ -1,33 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace GrumpEngine
 {
     class Tester
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
-
-            string text = "In the beginning. There was a Grump. A BIG Grump!!\nOwen didn't know how to encounter this problem because he was too smooth brain. So he woke up with an idea!\nHe grabbed his boots and ran out the door. Hopped on his bike and rode off a cliff.\nNow Owen is no longer smooth brain.";
-            foreach (char c in text)
-            {
-                Console.Write(c);
-                Thread.Sleep(2);
-            }
-            ResizeableMatrix rm = new ResizeableMatrix(6, 3);
-
-            rm.Fill(27);
-
-            Console.Write($"Matrix before add is - \n{rm}");
-
-            rm.Add(new EncounterType(4));
-
-            Console.WriteLine($"Matrix after add is - \n{rm}");
-
-            Console.ReadKey();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            TopLayerShell form = new TopLayerShell();
+            Application.Run(form);
         }
     }
 }
---
+++ GrumpEngine/TopLayerShell.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GrumpEngine
+{
+    public partial class TopLayerShell : Form
+    {
+        public TopLayerShell()
+        {
+            InitializeComponent();
+        }
+
+        public void SetTextBox(string input)
+        {
+            textBox2.AppendText(input);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void form1_Load(object sender, EventArgs e)
+        {
+            string text = "In the beginning. There was a Grump. A BIG Grump!! Owen didn't know how to encounter this problem because he was too smooth brain. So he woke up with an idea! He grabbed his boots and ran out the door. Hopped on his bike and rode off a cliff. Now Owen is no longer smooth brain.";
+            TypeTextToWindow(text);
+        }
+
+        private void TypeTextToWindow(string text)
+        {
+            foreach (char c in text)
+            {
+                SetTextBox(c.ToString());
+                Thread.Sleep(2);
+            }
+        }
+    }
+}
