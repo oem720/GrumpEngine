4b202c7546566421342e238006e092146961275d
Cameron Taylor <CDTaylor2@student.fullsail.edu>
2020-12-14 21:12:07 -0500

    Added code to Inventory.cs

--- GrumpEngine/Inventory.cs
+++ GrumpEngine/Inventory.cs
@@ -8,5 +8,29 @@ namespace GrumpEngine
 {
     class Inventory
     {
+        private int size;
+        private Entity[] inv;
+        public Inventory()
+        {
+            size = 0;
+            inv = new Entity[0];
+        }
+        public Inventory(int input)
+        {
+            size = input;
+            inv = new Entity[input];
+        }
+        public int GetSize()
+        {
+            return size;
+        }
+        public Entity[] GetInventory()
+        {
+            return inv;
+        }
+        public void addItem(Entity e)
+        {
+            e.GetWeight();
+        }
     }
 }
