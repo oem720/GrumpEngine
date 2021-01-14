da028cbdc788c9f45213330b3d38bcbfb784f3b0
Owen Meyers <OEMeyers@student.fullsail.edu>
2021-01-13 22:48:14 -0500

    Restructured the Grump Engine into an actual engine rather than an overcomplicated text parser

--- GrumpInterface/Program.cs
+++ GrumpInterface/Program.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.IO;
+﻿using GrumpEngine;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace GrumpEngine
+namespace GrumpInterface
 {
-    class Tester
+    static class Program
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
-            File.Create(Directory.GetCurrentDirectory() + "\\Config.cfg");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TopLayerShell());
-            
         }
     }
 }
