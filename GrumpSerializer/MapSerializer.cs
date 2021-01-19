abf48a2e1a240fd6afab0a3866790e59c72d3bee
Owen Meyers <OEMeyers@student.fullsail.edu>
2021-01-19 18:27:48 -0500

    Serializer added

---
+++ GrumpSerializer/MapSerializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace GrumpSerializer
+{
+    public class MapSerializer : IGrumpSerializer
+    {
+        private StreamWriter _writer;
+        private BinaryFormatter _bf;
+        private string _filepath;
+
+        public MapSerializer(StreamWriter sw, string filepath)
+        {
+            _writer = sw;
+            _bf = new BinaryFormatter();
+            _filepath = filepath;
+        }
+
+        public void Serialize(object ob)
+        {
+            
+        }
+
+
+    }
+}
---
+++ GrumpSerializer/SaveSerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrumpSerializer
+{
+    public class SaveSerializer : IGrumpSerializer
+    {
+        public void Serialize(object obj)
+        {
+
+        }
+    }
+}
