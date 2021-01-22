44ab3b3556539d51989b4b8c2bbd1cfdd6d3bfb2
Cameron Taylor <CDTaylor2@student.fullsail.edu>
2021-01-21 19:02:41 -0500

    Removed test line of code from World.cs

--- GrumpMapEditor/World.cs
+++ GrumpMapEditor/World.cs
@@ -8,7 +8,6 @@ namespace GrumpMapEditor
         [STAThread]
         static void Main()
         {
-            //var result = MessageBox.Show("Hi", "Help", MessageBoxButtons.YesNo);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TopLevelUI());
