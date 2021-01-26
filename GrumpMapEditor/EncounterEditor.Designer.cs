50672b2094e0e3d2e95f57beb70c67fa2908a847
Cameron Taylor <CDTaylor2@student.fullsail.edu>
2021-01-26 02:07:30 -0500

    Added tabs to encountereditor

--- GrumpMapEditor/EncounterEditor.Designer.cs
+++ GrumpMapEditor/EncounterEditor.Designer.cs
@@ -42,8 +42,12 @@
             this.entityAdderButton = new System.Windows.Forms.Button();
             this.entityTypeSelectorLabel = new System.Windows.Forms.Label();
             this.entityParameterText = new System.Windows.Forms.Label();
-            this.properties = new System.Windows.Forms.Button();
             this.storedValuesDisplayBox = new System.Windows.Forms.TreeView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // descriptorTagSelector
@@ -51,7 +55,7 @@
             this.descriptorTagSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.descriptorTagSelector.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.descriptorTagSelector.FormattingEnabled = true;
-            this.descriptorTagSelector.Location = new System.Drawing.Point(12, 324);
+            this.descriptorTagSelector.Location = new System.Drawing.Point(9, 283);
             this.descriptorTagSelector.Name = "descriptorTagSelector";
             this.descriptorTagSelector.Size = new System.Drawing.Size(121, 21);
             this.descriptorTagSelector.TabIndex = 0;
@@ -60,17 +64,17 @@
             // outputConsole
             // 
             this.outputConsole.Cursor = System.Windows.Forms.Cursors.No;
-            this.outputConsole.Location = new System.Drawing.Point(9, 27);
+            this.outputConsole.Location = new System.Drawing.Point(3, 19);
             this.outputConsole.Multiline = true;
             this.outputConsole.Name = "outputConsole";
             this.outputConsole.ReadOnly = true;
-            this.outputConsole.Size = new System.Drawing.Size(320, 219);
+            this.outputConsole.Size = new System.Drawing.Size(308, 180);
             this.outputConsole.TabIndex = 1;
             // 
             // outputLabel
             // 
             this.outputLabel.AutoSize = true;
-            this.outputLabel.Location = new System.Drawing.Point(10, 7);
+            this.outputLabel.Location = new System.Drawing.Point(3, 3);
             this.outputLabel.Name = "outputLabel";
             this.outputLabel.Size = new System.Drawing.Size(80, 13);
             this.outputLabel.TabIndex = 3;
@@ -79,17 +83,18 @@
             // displayEncounterReg
             // 
             this.displayEncounterReg.AutoSize = true;
-            this.displayEncounterReg.Location = new System.Drawing.Point(335, 7);
+            this.displayEncounterReg.Location = new System.Drawing.Point(314, 3);
             this.displayEncounterReg.Name = "displayEncounterReg";
             this.displayEncounterReg.Size = new System.Drawing.Size(152, 13);
             this.displayEncounterReg.TabIndex = 4;
             this.displayEncounterReg.Text = "Encounter Type Stored Values";
+            this.displayEncounterReg.Click += new System.EventHandler(this.displayEncounterReg_Click);
             // 
             // submitChangeButton
             // 
-            this.submitChangeButton.Location = new System.Drawing.Point(9, 252);
+            this.submitChangeButton.Location = new System.Drawing.Point(3, 204);
             this.submitChangeButton.Name = "submitChangeButton";
-            this.submitChangeButton.Size = new System.Drawing.Size(320, 41);
+            this.submitChangeButton.Size = new System.Drawing.Size(308, 41);
             this.submitChangeButton.TabIndex = 5;
             this.submitChangeButton.Text = "Save and Exit";
             this.submitChangeButton.UseVisualStyleBackColor = true;
@@ -98,7 +103,7 @@
             // descriptorTagSelectorLabel
             // 
             this.descriptorTagSelectorLabel.AutoSize = true;
-            this.descriptorTagSelectorLabel.Location = new System.Drawing.Point(9, 306);
+            this.descriptorTagSelectorLabel.Location = new System.Drawing.Point(6, 251);
             this.descriptorTagSelectorLabel.Name = "descriptorTagSelectorLabel";
             this.descriptorTagSelectorLabel.Size = new System.Drawing.Size(77, 13);
             this.descriptorTagSelectorLabel.TabIndex = 6;
@@ -107,7 +112,7 @@
             // descriptorAdderButton
             // 
             this.descriptorAdderButton.Enabled = false;
-            this.descriptorAdderButton.Location = new System.Drawing.Point(536, 351);
+            this.descriptorAdderButton.Location = new System.Drawing.Point(513, 309);
             this.descriptorAdderButton.Name = "descriptorAdderButton";
             this.descriptorAdderButton.Size = new System.Drawing.Size(121, 23);
             this.descriptorAdderButton.TabIndex = 7;
@@ -117,16 +122,16 @@
             // 
             // descriptorText
             // 
-            this.descriptorText.Location = new System.Drawing.Point(170, 325);
+            this.descriptorText.Location = new System.Drawing.Point(173, 283);
             this.descriptorText.Name = "descriptorText";
-            this.descriptorText.Size = new System.Drawing.Size(487, 20);
+            this.descriptorText.Size = new System.Drawing.Size(461, 20);
             this.descriptorText.TabIndex = 8;
             this.descriptorText.TextChanged += new System.EventHandler(this.DescriptorText_TextChanged);
             // 
             // descriptorTextLabel
             // 
             this.descriptorTextLabel.AutoSize = true;
-            this.descriptorTextLabel.Location = new System.Drawing.Point(167, 306);
+            this.descriptorTextLabel.Location = new System.Drawing.Point(170, 251);
             this.descriptorTextLabel.Margin = new System.Windows.Forms.Padding(3);
             this.descriptorTextLabel.Name = "descriptorTextLabel";
             this.descriptorTextLabel.Size = new System.Drawing.Size(79, 13);
@@ -138,16 +143,16 @@
             this.entityTypeSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.entityTypeSelector.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.entityTypeSelector.FormattingEnabled = true;
-            this.entityTypeSelector.Location = new System.Drawing.Point(12, 417);
+            this.entityTypeSelector.Location = new System.Drawing.Point(9, 383);
             this.entityTypeSelector.Name = "entityTypeSelector";
             this.entityTypeSelector.Size = new System.Drawing.Size(121, 21);
             this.entityTypeSelector.TabIndex = 11;
             // 
             // entityParameterEntry
             // 
-            this.entityParameterEntry.Location = new System.Drawing.Point(170, 418);
+            this.entityParameterEntry.Location = new System.Drawing.Point(173, 383);
             this.entityParameterEntry.Name = "entityParameterEntry";
-            this.entityParameterEntry.Size = new System.Drawing.Size(487, 20);
+            this.entityParameterEntry.Size = new System.Drawing.Size(461, 20);
             this.entityParameterEntry.TabIndex = 12;
             this.entityParameterEntry.Text = "Temp text box (will be replaced with the final parameters after the baseline enti" +
     "ties are done)";
@@ -155,7 +160,7 @@
             // entityAdderButton
             // 
             this.entityAdderButton.Enabled = false;
-            this.entityAdderButton.Location = new System.Drawing.Point(536, 444);
+            this.entityAdderButton.Location = new System.Drawing.Point(513, 407);
             this.entityAdderButton.Name = "entityAdderButton";
             this.entityAdderButton.Size = new System.Drawing.Size(121, 23);
             this.entityAdderButton.TabIndex = 13;
@@ -165,7 +170,7 @@
             // entityTypeSelectorLabel
             // 
             this.entityTypeSelectorLabel.AutoSize = true;
-            this.entityTypeSelectorLabel.Location = new System.Drawing.Point(12, 398);
+            this.entityTypeSelectorLabel.Location = new System.Drawing.Point(6, 353);
             this.entityTypeSelectorLabel.Name = "entityTypeSelectorLabel";
             this.entityTypeSelectorLabel.Size = new System.Drawing.Size(60, 13);
             this.entityTypeSelectorLabel.TabIndex = 14;
@@ -174,28 +179,63 @@
             // entityParameterText
             // 
             this.entityParameterText.AutoSize = true;
-            this.entityParameterText.Location = new System.Drawing.Point(170, 399);
+            this.entityParameterText.Location = new System.Drawing.Point(170, 353);
             this.entityParameterText.Name = "entityParameterText";
             this.entityParameterText.Size = new System.Drawing.Size(89, 13);
             this.entityParameterText.TabIndex = 15;
             this.entityParameterText.Text = "Entity Parameters";
             // 
-            // properties
-            // 
-            this.properties.Location = new System.Drawing.Point(338, 252);
-            this.properties.Name = "properties";
-            this.properties.Size = new System.Drawing.Size(318, 40);
-            this.properties.TabIndex = 16;
-            this.properties.Text = "Properties";
-            this.properties.UseVisualStyleBackColor = true;
-            this.properties.Click += new System.EventHandler(this.PropertyButtonClick);
-            // 
             // storedValuesDisplayBox
             // 
-            this.storedValuesDisplayBox.Location = new System.Drawing.Point(339, 27);
+            this.storedValuesDisplayBox.Location = new System.Drawing.Point(317, 19);
             this.storedValuesDisplayBox.Name = "storedValuesDisplayBox";
-            this.storedValuesDisplayBox.Size = new System.Drawing.Size(317, 218);
+            this.storedValuesDisplayBox.Size = new System.Drawing.Size(317, 180);
             this.storedValuesDisplayBox.TabIndex = 17;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(9, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(648, 457);
+            this.tabControl1.TabIndex = 18;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Gainsboro;
+            this.tabPage1.Controls.Add(this.outputConsole);
+            this.tabPage1.Controls.Add(this.submitChangeButton);
+            this.tabPage1.Controls.Add(this.entityAdderButton);
+            this.tabPage1.Controls.Add(this.storedValuesDisplayBox);
+            this.tabPage1.Controls.Add(this.descriptorAdderButton);
+            this.tabPage1.Controls.Add(this.outputLabel);
+            this.tabPage1.Controls.Add(this.displayEncounterReg);
+            this.tabPage1.Controls.Add(this.descriptorTagSelectorLabel);
+            this.tabPage1.Controls.Add(this.entityParameterText);
+            this.tabPage1.Controls.Add(this.entityParameterEntry);
+            this.tabPage1.Controls.Add(this.descriptorTagSelector);
+            this.tabPage1.Controls.Add(this.entityTypeSelectorLabel);
+            this.tabPage1.Controls.Add(this.descriptorTextLabel);
+            this.tabPage1.Controls.Add(this.entityTypeSelector);
+            this.tabPage1.Controls.Add(this.descriptorText);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(640, 431);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Editor";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(640, 433);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Properties";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // EncounterEditor
             // 
@@ -203,29 +243,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(669, 481);
-            this.Controls.Add(this.storedValuesDisplayBox);
-            this.Controls.Add(this.properties);
-            this.Controls.Add(this.entityParameterText);
-            this.Controls.Add(this.entityTypeSelectorLabel);
-            this.Controls.Add(this.entityAdderButton);
-            this.Controls.Add(this.entityParameterEntry);
-            this.Controls.Add(this.entityTypeSelector);
-            this.Controls.Add(this.descriptorTextLabel);
-            this.Controls.Add(this.descriptorText);
-            this.Controls.Add(this.descriptorAdderButton);
-            this.Controls.Add(this.descriptorTagSelectorLabel);
-            this.Controls.Add(this.submitChangeButton);
-            this.Controls.Add(this.displayEncounterReg);
-            this.Controls.Add(this.outputLabel);
-            this.Controls.Add(this.outputConsole);
-            this.Controls.Add(this.descriptorTagSelector);
+            this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "EncounterEditor";
             this.Text = "Encounter Editor";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.EncounterEditor_FormClosing);
             this.Load += new System.EventHandler(this.Editor_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -245,7 +272,9 @@
         private System.Windows.Forms.Button entityAdderButton;
         private System.Windows.Forms.Label entityTypeSelectorLabel;
         private System.Windows.Forms.Label entityParameterText;
-        private System.Windows.Forms.Button properties;
         private System.Windows.Forms.TreeView storedValuesDisplayBox;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
