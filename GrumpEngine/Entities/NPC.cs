1341a373e19656aa27be47adbea25f516d5769e8
Owen Meyers <OEMeyers@student.fullsail.edu>
2021-01-26 02:07:33 -0500

    Added NPC class

--- GrumpEngine/Entities/NPC.cs
+++ GrumpEngine/Entities/NPC.cs
@@ -7,8 +7,26 @@ using System.Threading.Tasks;
 namespace GrumpEngine
 {
     [Serializable]
-    public class NPC
+    public class NPC : ICharacter
     {
+        //implement the required properties for ICharacter
+        public int Health { get; set; }
+        public float DamageResistance { get; private set; }
+        public int Level { get; private set; }
+        public DialogueTree Dialogue { get; private set; } = Factory.ConstructDialogueTree();
+        public bool IsTalkable { get; private set; }
+        public Inventory Inv { get; private set; } = Factory.ConstructInventory(25);
+        public Weapon CurrentEquippedWeapon { get; private set; }
+        public Armor CurrentEquippedArmor { get; private set; }
 
+        //implement the required properties for IEntity
+        public float Weight { get; private set; } = 0.0f;
+        public string Identifier { get; private set; } = "Enemy";
+        public bool IsGrabbable { get; set; } = false;
+
+        public void AddDialogue(DescriptorString ds)
+        {
+            Dialogue.AddString(ds);
+        }
     }
 }
